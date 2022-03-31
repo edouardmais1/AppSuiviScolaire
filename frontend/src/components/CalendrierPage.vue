ab6fe6b29c10b9d5b132ca88b9cd095182f2e683
M-Momin <m.momin@students.ephec.be>
2022-03-31 15:28:19 +0200

    modification de calendrier Frontend

--- frontend/src/components/CalendrierPage.vue
+++ frontend/src/components/CalendrierPage.vue
@@ -3,6 +3,8 @@ import '@fullcalendar/core/vdom' // solves problem with Vite
 import FullCalendar from '@fullcalendar/vue3'
 import dayGridPlugin from '@fullcalendar/daygrid'
 import interactionPlugin from '@fullcalendar/interaction'
+import listPlugin from '@fullcalendar/list'
+import timeGridPlugin from '@fullcalendar/timegrid'
 
 export default {
   components: {
@@ -11,14 +13,33 @@ export default {
   data() {
     return {
       calendarOptions: {
-        plugins: [ dayGridPlugin, interactionPlugin ],
+        plugins: [dayGridPlugin, interactionPlugin, listPlugin, timeGridPlugin],
+        locale: 'fr',
+        buttonText: {
+          today:    'Aujourd\'hui',
+          month:    'Mois',
+          week:     'Semaine',
+          day:      'Jour',
+          list:     'Liste'
+        },
+        themeSystem: 'standard',
         initialView: 'dayGridMonth',
-        dateClick: this.handleDateClick,
+        headerToolbar: {
+          left: 'today prev,next',
+          center: 'title',
+          right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek'
+        },
+        dateClick: function(arg) {
+          if(arg.view.type=="dayGridMonth"){
+            this.changeView("timeGridDay",arg.dateStr);
+          }
+        },
         events: [
             //Ajouter une ligne title et date.
           { title: 'event 1', date: '2022-03-30' },
           { title: 'event 2', date: '2022-03-30' }
         ],
+        eventColor: '#6dabe4',
         eventClick: function(info) {
             info.jsEvent.preventDefault(); // don't let the browser navigate
 
@@ -29,13 +50,10 @@ export default {
       }
     }
   },
-  methods: {
-    handleDateClick: function(arg) {
-      alert('date click! ' + arg.dateStr)
-    }
-  }
 }
 </script>
+
+
 <template>
 <div class = "calendar">
 
@@ -45,8 +63,15 @@ export default {
 </template>
 
 <style>
-    .calendar{
-        margin: 10% 10%; 
+#calendar {
+  max-width: 900px;
+  margin: 40px auto;
+}
+.initialView{
+  font-color: #6dabe4;
+}
+.calendar{
+        margin: 10% 10%;
         border: 3px solid #6dabe4;
         box-shadow: 0px 15px 16.83px 0.17px rgba(0, 0, 0, 0.05);
         -moz-box-shadow: 0px 15px 16.83px 0.17px rgba(0, 0, 0, 0.05);
@@ -59,5 +84,6 @@ export default {
         -o-border-radius: 20px;
         -ms-border-radius: 20px;
         padding: 25px;
-    }
+        font-size: 14px;
+}
 </style>
